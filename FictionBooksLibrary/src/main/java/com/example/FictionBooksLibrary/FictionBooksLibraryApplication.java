a0b8d34609481275b5fe71b90d854fdffe4b1888
dimplekataria <kataria.dimple@gmail.com>
2017-07-06 00:46:07 -0400

    New project
    Test to learn Git and spring boot attributes

---
+++ FictionBooksLibrary/src/main/java/com/example/FictionBooksLibrary/FictionBooksLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.example.FictionBooksLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FictionBooksLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FictionBooksLibraryApplication.class, args);
+	}
+}
